e080ed199ac12e23ff69ebbc433f8044e9b40361
Abdur Razzak <abdur.razzak@ctrends-software.com>
2019-07-09 00:34:09 +0600

    Packet-tracer pactice files, notes, documents added

---
+++ CCNA/Bionic/net/Ethernet/What is Ethernet  - Definition from WhatIs.com_files/ca-pub-6050985421795229.js
@@ -0,0 +1,1 @@
+(google_async_config = window.google_async_config || {})['ca-pub-6050985421795229'] = {"sra_enabled":false};
---
+++ CCNA/Bionic/net/Ethernet/What is Ethernet  - Definition from WhatIs.com_files/geo2.js
@@ -0,0 +1,16 @@
+(function(){
+  window['optimizely'] = window['optimizely'] || [];
+  window['optimizely'].push(['activateGeoDelayedExperiments', {
+    'location':{
+      'city': "DHAKA",
+      'continent': "AS",
+      'country': "BD",
+      'region': ""
+    },
+    'ip':"119.30.35.144"
+  }]);
+})
+//
+()
+
+;
